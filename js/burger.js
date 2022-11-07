5093ef4a6cdb5cc6d38f043777292d9b7dbcfa65
AnnaVishnyakova <simonya6ka@gmail.com>
2022-11-07 11:31:18 +0300

    version 2

---
+++ js/burger.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('.burger').click(function() {
+        
+        $('.burger__menu').addClass('open-menu');
+        $("body").addClass("fixed");
+    });
+    
+    $('.burger__close').click(function() {
+        $('.burger__menu').removeClass('open-menu');
+        $("body").removeClass("fixed");
+    });
+    $('.burger__item-link').click(function() {
+        $('.burger__menu').removeClass('open-menu');
+        $("body").removeClass("fixed");
+    });
+
+   
+  
+});
+
+$(document).mouseup(function (e) {
+    var burger = $(".burger__wrapper");
+    if (burger.has(e.target).length === 0){
+		$('.burger__menu').removeClass('open-menu');
+		$('body').removeClass('fixed')
+    }
+});
+
+
